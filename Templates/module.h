b1d268fd2574f0e6b896c17917b0de10b342fb0c
Christian Sandoval <san16250@uvg.edu.gt>
2020-10-22 12:37:52 -0600

    Added the templates of the Barr coding standard for creating new modules.

---
+++ Templates/module.h
@@ -0,0 +1,16 @@
+/** @file module.h
+ *
+ * @brief A description of the module’s purpose.
+ *
+ * @par
+ * COPYRIGHT NOTICE: (c) 2018 Barr Group. All rights reserved.
+ */
+
+#ifndef MODULE_H
+#define MODULE_H
+
+int8_t max8(int8_t num1, int8_t num2);
+
+#endif /* MODULE_H */
+
+/*** end of file ***/
